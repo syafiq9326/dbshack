8157ba68c0c428cac32b7c57353eec33242a3bb1
sarang2551 <sarang.nirwan@gmail.com>
2025-01-11 12:47:09 +0800

    Adding database models

---
+++ backend/models/companyaccount.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const companyAccountSchema = new mongoose.Schema({
+    companyName: { type: String, required: true, unique: true },
+    activeAccount: { type: Boolean, required: true },
+    carbonBalance: { type: Number, required: true },
+    cashBalance: { type: Number, required: true },
+    outstandingRequests: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "OutstandingRequest" }, // References to OutstandingRequest
+    ],
+    createdDatetime: { type: Date, default: Date.now },
+    updatedDatetime: { type: Date, default: Date.now },
+});
+
+const CompanyAccount = mongoose.model("CompanyAccount", companyAccountSchema);
+module.exports = CompanyAccount;
