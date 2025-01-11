7b6b7d7aedb93d13d5ff865d110900fff1c4d432
spiritoftime <chooliangyi6@gmail.com>
2025-01-11 11:27:24 +0800

    add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserProvider } from './contexts/userContext';
+import { UserProvider } from "./contexts/userContext";
 // import Navbar from './components/navbar/navbar';
-import Login from './pages/login';
-import Productlist from './pages/protected/productlist';
+import Login from "./pages/login";
+import Productlist from "./pages/protected/productlist";
+import RequestsReceivedPage from "./pages/requestsReceived";
+import LandingPage from "./pages/landing";
 
 const App = () => {
   return (
-    <UserProvider> 
+    <UserProvider>
       <Router>
-          {/* <Navbar /> */}
-          <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/productlist" element={<Productlist />} />
-          </Routes>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/requests-received" element={<RequestsReceivedPage />} />
+          <Route path="/home" element={<LandingPage />} />
+
+          <Route path="/" element={<Login />} />
+          <Route path="/productlist" element={<Productlist />} />
+        </Routes>
       </Router>
     </UserProvider>
   );
 };
 
 export default App;
-
-
